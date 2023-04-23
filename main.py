325d5dbff088b54bbbdc88e43d825e6b3d7b2f9c
mihailermak <122021329+mihailermak@users.noreply.github.com>
2023-04-23 10:31:24 +0300

    Добавление конвертаций

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #Token: 6183220089:AAF92022c-KAO_EAAcF3_TnsEfg5IxX0Rug
 
 import logging
+import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+
+#Запрос к openexchangerates.org
+url = "https://openexchangerates.org/api/latest.json?app_id=36011ff3ad1d4279a3dcd70b5c8ad104"
+response = requests.get(url)
+rates = response.json()["rates"]
 
 #Устанавливаем уровень логирования
 logging.basicConfig(level=logging.INFO)
@@ -11,17 +17,33 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token='6183220089:AAF92022c-KAO_EAAcF3_TnsEfg5IxX0Rug')
 dp = Dispatcher(bot)
 
+#Список поддерживаемых валют
+currencyies = ('USD', 'EUR', 'RUB', 'GBP', 'AED', 'ANG', 'AMD',
+               'AZN', 'BAM', 'BYN', 'CAD', 'CHF', 'CLP', 'CNY',
+               'COP', 'CRC', 'CUP', 'CZK', 'DKK', 'EEK', 'EGP',
+               'HKD', 'IDR', 'INR', 'IRR', 'ISK', 'JPY', 'KGS',
+               'KRW', 'KZT', 'MAD', 'MDL', 'MNT', 'MXN', 'NOK',
+               'NZD', 'PHP', 'PKR', 'PLN', 'QAR', 'RON', 'RSD',
+               'SAR', 'SEK', 'SGD', 'SYP', 'THB', 'TJS', 'TMT',
+               'TRY', 'UAH', 'USD', 'UZS', 'VES', 'ZAR')
+
 #Обработка команды /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     #Создаем клавиатуру с двумя кнопками
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button_conversion = types.KeyboardButton(text="Конвертация")
-    button_settings = types.KeyboardButton(text="Настройки")
+    button_conversion = types.KeyboardButton(text="конвертация")
+    button_settings = types.KeyboardButton(text="настройки")
     keyboard.add(button_conversion, button_settings)
 
     #Отправляем приветственное сообщение с клавиатурой
     await message.answer("Привет! Я телеграмм бот для конвертации валют. Что будем делать?", reply_markup=keyboard)
+
+@dp.message_handler(text='конвертация')
+async def convertation(message: types.Message):
+    #Конвертируем валюту
+    converted_amount = (int(100) / rates["USD"] * rates["EUR"])
+    await message.answer(text=(f'{100} {"USD"} = {converted_amount} {"EUR"}'))
 
 #Запускаем бота
 if __name__ == '__main__':
